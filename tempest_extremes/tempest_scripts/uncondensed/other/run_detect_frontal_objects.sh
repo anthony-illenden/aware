4c25a3fd18e6f54b64d022d8d9ee620a65b765ca
Tony Illenden <tonyillenden@gmail.com>
2025-08-22 08:11:54 -0700

    minor changes

---
+++ tempest_extremes/tempest_scripts/uncondensed/other/run_detect_frontal_objects.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name="tempest_extremes"
+#SBATCH --output="tempest_extremes.%j.%N.out.txt"
+#SBATCH --partition=compute-192
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --export=ALL
+#SBATCH --account=csg101
+#SBATCH --time=1:00:00
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=aillenden@ucsd.edu
+#######################################################
+csh /cw3e/mead/projects/csg101/aillenden/tempest_extremes/tempest_scripts/uncondensed/frontal_object_detect_blobs.csh
+
+rm -f /cw3e/mead/projects/csg101/aillenden/tempest_extremes/tempest_scripts/uncondensed/other/log*.txt
